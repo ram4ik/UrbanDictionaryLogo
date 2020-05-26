1d395ecf6441e2959282649ece3b0c4f2abf95ae
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-05-26 10:35:20 +0300

    init

--- UrbanDictionaryLogo/ContentView.swift
+++ UrbanDictionaryLogo/ContentView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color("dDark"), Color("dLight")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            ZStack {
+                Text("U")
+                    .foregroundColor(Color("bdDark"))
+                    .offset(x: -120, y: -100)
+                Text("D")
+                    .foregroundColor(Color("bdDark"))
+                        .offset(x: 120, y: 100)
+            }.font(.system(size: 800))
+                .shadow(color: Color.white, radius: 20, x: 0, y: 0)
+                    
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
