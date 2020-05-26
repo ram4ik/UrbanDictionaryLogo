04e18e7a58c2038be6167ff29b2321ba7ecd0ec5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-05-26 10:08:36 +0300

    Initial Commit

---
+++ UrbanDictionaryLogo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UrbanDictionaryLogo
+//
+//  Created by ramil on 26.05.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
